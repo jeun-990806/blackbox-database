e68fdabc33936bc36998487b3485d11b94f587ec
jieun Kim <kjeun990806@gmail.com>
2022-06-04 22:51:33 +0900

    게시글 삭제 기능 구현

--- controllers/view.js
+++ controllers/view.js
@@ -5,3 +5,15 @@ module.exports.renderer = async (request, response) => {
     const post_data = await post.findById(request.params.post_id)
     response.render('view', {post_data: post_data, moment: moment, logined_user: request.session.email})
 }
+
+module.exports.delete_validate = async (request, response, next) => {
+    const target_post = await post.findOne({ _id: request.params.post_id, writer: request.session.email} )
+    if(!target_post) return response.redirect('/error')
+
+    next()
+}
+
+module.exports.delete = async (request, response) => {
+    await post.deleteOne({ _id: request.params.post_id} )
+    response.redirect('/board/questions')
+}
