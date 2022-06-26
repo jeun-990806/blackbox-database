fdd7ac014cdbf190759c8537335edd5275827340
jeun-990806 <kjeun990806@gmail.com>
2022-06-26 13:40:52 +0900

    heroku 배포

--- test.js
+++ test.js
@@ -1,42 +1,66 @@
-const selenium = require('selenium-webdriver')
-const chrome = require('selenium-webdriver/chrome')
-const problem = require('./models/problem')
+const child_process = require('child_process')
+const fs = require('fs')
 
 module.exports = async (request, response) => {
-    problem.findOne({number: request.params.id}, async (error, problem_info) => {
-        if(!problem_info){
-            const url = 'https://www.acmicpc.net/problem/' + request.params.id
-            const service = new chrome.ServiceBuilder('./chromedriver.exe').build()
-            try {
-                chrome.setDefaultService(service);
-            } catch (error) {
-                console.log(error)
-            }
-            
-            const driver = new selenium.Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().headless()).build()
-            try {
-                await driver.get(url)
-                const title = await driver.findElement(selenium.By.css('#problem_title')).getText()
-                const answer_ratio = await driver.findElement(selenium.By.css('#problem-info > tbody > tr > td:nth-child(6)')).getText()
-                problem.create({
-                    source_site: 'baekjoon',
-                    number: request.params.id,
-                    title: title,
-                    answer_ratio: answer_ratio.split('%')[0],
-                    link: 'https://www.acmicpc.net/problem/' + request.params.id
-                })
-                response.write('문제를 긁어왔습니다!')
-                return response.end()
-            } catch (error) {
-                console.log(error)
-            } finally {
-                await driver.quit()
-            }
-        }else{
-            response.write('이미 긁어온 문제예요!')
-            return response.end()
-        }
-    })
+    const source_code =
+`
+#include <stdio.h>
 
+int main(int argc, char *argv){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d", a+b);
+
+    return 0;
+}
+`
+    const input = '1 2'
+    const output = '4'
+    var real_output = ''
     
+    const tmp_file_name = 'tmp_code'
+
+    make_c_file(tmp_file_name, source_code)
+    compile(tmp_file_name)
+    
+    response.write('input: ' + input)
+    response.write('\nexpected output: ' + output)
+
+    real_output = await execute(tmp_file_name + '.exe', input)
+    response.write('\nreal output: ' + real_output)
+    if(output === real_output) response.write('\ngood!\n')
+    else response.write('\nwrong...\n')
+    response.end()
+}
+
+const get_source = async (problem_id) => {
+    await source.findOne({problem_id: problem_id}, 
+        (error, result) => {
+            if(error) console.log(error)
+            return result.content
+        }
+    )
+}
+
+const make_c_file = (file_name, source_code) => {
+    fs.writeFileSync(file_name + '.c', source_code, 'utf-8', (error, stdout, stderr) => {
+        if(error) console.log(error)
+        if(stdout) console.log(stdout)
+        if(stderr) console.log(stderr)
+    })
+}
+
+const compile = (file_name) => {
+    child_process.execSync('gcc ' + file_name + '.c -o ' + file_name + '.exe')
+}
+
+const execute = (file_name, input) => {
+    return new Promise((resolve, reject) => {
+        const child = child_process.spawn(file_name)
+        child.stdin.write(input)
+        child.stdin.end()
+
+        child.stdout.on('data', (data) => { resolve(data + '') } )
+    });
 }
